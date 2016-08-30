fb2770c14abc365e125940fbb5e5ade112a61d91
jeulindev <svagnier@jeulin.fr>
2016-08-30 09:03:53 +0200

    ..
    ..

--- GroomyScratch/groomy_extension_fr.js
+++ GroomyScratch/groomy_extension_fr.js
@@ -549,7 +549,7 @@ $ip = "10.0.2.221";//
 			//--
 			['-'],
 			['w', 'Affecter %m.DigitalValues à sortie numérique %m.DigitalNumber', 'setDOutput', 0, 1],
-			['w', 'Affecter %m.DigitalNumber à sortie numérique %m.DigitalNumber', 'setDOutput2', '  0', 1],
+			['w', 'Affecter %m.DigitalNumber à sortie numérique %m.DigitalNumber', 'setDOutput2', 0, 1],
 			['w', 'Affecter %s à sortie analogique %m.AnalogNumber2', 'setAOutput', '  0', 1],		
 			['w', 'Affecter %m.DigitalValues à relais %m.AnalogNumber4 ', 'setROutput', 0, 1 ],		
 			['w', 'Affecter %s à relais %m.AnalogNumber4 ', 'setROutput2', '0', 1 ],		
